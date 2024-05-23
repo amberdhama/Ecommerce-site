7254c895b4b4f958506ba2f1d89e6d2217c111c8
amber <fauxade007@gmail.com>
2024-05-23 12:51:18 +0530

    up1

--- src/Components/Featured Products/rows.jsx
+++ src/Components/Featured Products/rows.jsx
@@ -1,8 +1,9 @@
+
 const row1=[
 {
     id:1,
     amount: 32,
-    img: "../../../Images/images/product-1.jpg", 
+    img: "../../../images2/product-1.jpg", 
     prod_name: "Red Printed Tshirt", 
     fourth: "fa-solid fa-star",
     fifth: "fa-regular fa-star"
@@ -10,7 +11,7 @@ const row1=[
 {
     id: 2,
     amount: 45,
-    img: "../../../Images/images/product-2.jpg", 
+    img: "../../../images2/product-2.jpg", 
     prod_name: "Black sports shoes", 
     fourth: "fa-solid fa-star-half-stroke",
     fifth: "fa-regular fa-star"
@@ -18,7 +19,7 @@ const row1=[
 {
     id:3,
     amount: 39.9,
-    img: "../../../Images/images/product-3.jpg", 
+    img: "../../../images2/product-3.jpg", 
     prod_name: "Grey lower", 
     fourth: "fa-solid fa-star-half-stroke",
     fifth: "fa-regular fa-star"
@@ -26,7 +27,7 @@ const row1=[
 {
     id:4,
     amount: 54,
-    img: "../../../Images/images/product-4.jpg", 
+    img: "../../../images2/product-4.jpg", 
     prod_name: "Blue Tshirt", 
     fourth: "fa-solid fa-star",
     fifth: "fa-regular fa-star"
@@ -35,7 +36,7 @@ const row1=[
     {
         id:5,
         amount: 36,
-        img: "../../../Images/images/product-5.jpg", 
+        img: "../../../images2/product-5.jpg", 
         prod_name: "Light Grey Sneakers", 
         fourth: "fa-solid fa-star",
         fifth: "fa-regular fa-star"
@@ -43,7 +44,7 @@ const row1=[
     {
         id:6,
         amount: 45,
-        img: "../../../Images/images/product-6.jpg", 
+        img: "../../../images2/product-6.jpg", 
         prod_name: "Black Tshirt", 
         fourth: "fa-solid fa-star-half-stroke",
         fifth: "fa-regular fa-star"
@@ -51,7 +52,7 @@ const row1=[
     {
         id:7,
         amount: 9,
-        img: "../../../Images/images/product-7.jpg", 
+        img: "../../../images2/product-7.jpg", 
         prod_name: "Socks (Pack of 3)", 
         fourth: "fa-solid fa-star-half-stroke",
         fifth: "fa-regular fa-star"
@@ -59,7 +60,7 @@ const row1=[
     {
         id:8,
         amount: 15,
-        img: "../../../Images/images/product-8.jpg", 
+        img: "../../../images2/product-8.jpg", 
         prod_name: "Black Dial Watch", 
         fourth: "fa-solid fa-star",
         fifth: "fa-regular fa-star"
@@ -68,7 +69,7 @@ const row1=[
         {
             id:9,
             amount:20,
-            img: "../../../Images/images/product-9.jpg", 
+            img: "../../../images2/product-9.jpg", 
             prod_name: "Dark Grey Dial Watch", 
             fourth: "fa-solid fa-star",
             fifth: "fa-regular fa-star"
@@ -76,7 +77,7 @@ const row1=[
         {
             id:10,
             amount: 53,
-            img: "../../../Images/images/product-10.jpg", 
+            img: "../../../images2/product-10.jpg", 
             prod_name: "Black and Orange sports shoes", 
             fourth: "fa-solid fa-star-half-stroke",
             fifth: "fa-regular fa-star"
@@ -84,7 +85,7 @@ const row1=[
         {
             id:11,
             amount: 42,
-            img: "../../../Images/images/product-11.jpg", 
+            img: "../../../images2/product-11.jpg", 
             prod_name: "Grey Women Shoes", 
             fourth: "fa-solid fa-star-half-stroke",
             fifth: "fa-regular fa-star"
@@ -92,7 +93,7 @@ const row1=[
         {
             id:12,
             amount: 37,
-            img: "../../../Images/images/product-12.jpg", 
+            img: "../../../images2/product-12.jpg", 
             prod_name: "Black Lower", 
             fourth: "fa-solid fa-star",
             fifth: "fa-regular fa-star"
